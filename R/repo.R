fc54784861a3d435a655dfa65ce24bda53851117
Thomas J. Leeper <thosjleeper@gmail.com>
2015-11-12 10:31:23 +0000

    tweak code; add branch s3 print method (#1)

--- R/repo.R
+++ R/repo.R
@@ -38,8 +38,8 @@ print.travis_repo <- function(x, ...) {
 #' @title Get Branch
 #' @description Retrieve branches for a repo
 #' @details This can retrieve a list of recent branches for a given repo, or if \code{branch} is specified, details about a specific branch.
-#' @param repo Optionally, a numeric repository ID (such as returned by this function) or a character string specifying a GitHub repository \dQuote{slug} (e.g., \samp{ghusername/ghreponame}).
-#' @param branch A character string specifying the name of a branch.
+#' @param repo A numeric repository ID (such as returned by this function) or a character string specifying a GitHub repository \dQuote{slug} (e.g., \samp{ghusername/ghreponame}).
+#' @param branch Optionally, a character string specifying the name of a branch.
 #' @param ... Additional arguments passed to \code{\link{travisHTTP}}.
 #' @return A list.
 #' @seealso \code{\link{get_builds}}
@@ -57,21 +57,33 @@ print.travis_repo <- function(x, ...) {
 #' @seealso \code{\link{get_repo}}, \code{\link{get_builds}}
 #' @export
 get_branch <- function(repo, branch, ...) {
-    if (!missing(branch)) {
-        travisHTTP("GET", path = paste0("/repos/", repo, "/branches"), ...)
+    if (missing(branch)) {
+        out <- travisHTTP("GET", path = paste0("/repos/", repo, "/branches"), ...)
+        structure(list(branches = lapply(out$branches, `class<-`, "travis_branch"),
+                       commits = lapply(out$commits, `class<-`, "travis_commit")))
     } else {
         travisHTTP("GET", path = paste0("/repos/", repo, "/branches/", branch), ...)
+        structure(list(branch = `class<-`(out$branches, "travis_branch"),
+                       commits = `class<-`(out$commits, "travis_commit")))
     }
-    
+}
+
+print.travis_branch <- function(x, ...) {
+    cat("Branch (", x$id, ") state: ", x$state, "\n", sep = "")
+    cat("Repo:   ", x$repository_id, "\n", sep = "")
+    cat("Commit: ", x$commit_id, "\n", sep = "")
+    cat("Started:  ", x$started_at, "\n", sep = "")
+    cat("Finished: ", x$finished_at, "\n", sep = "")
+    invisible(x)
 }
 
 #' @title Get/Set Repo Settings
 #' @description Get or set repository settings
 #' @details \code{get_repo_settings} retrieves Travis-CI settings for a given repository. \code{set_repo_settings} modifies those settings based upon a list of input values. Use \code{\link{get_env_vars}} and \code{\link{add_env_vars}} to modify environment variables used in a build. Most of these features can also be modified using a \samp{.travis.yml} file in the GitHub repository itself (see \href{http://docs.travis-ci.com/user/languages/r/}{Building an R Project} in the Travis CI documentation for details).
 #' @param repo A numeric repository ID (such as returned by this function) or a character string specifying a GitHub repository \dQuote{slug} (e.g., \samp{ghusername/ghreponame}).
-#' @param settings A list containing named settings and their values. See results of \code{get_repo_settings} and \href{http://docs.travis-ci.com/api/#settings:-general}{the API documentation} for details.
+#' @param settings A list containing named settings and their values. See results of \code{get_repo_settings} and \href{http://docs.travis-ci.com/api/#settings:-general}{the API documentation} for details. The response from either \code{get_repo_settings} or \code{set_repo_settings} can be passed directly as the value of this argument.
 #' @param ... Additional arguments passed to \code{\link{travisHTTP}}.
-#' @return A list.
+#' @return A list of settings.
 #' @seealso \code{\link{get_builds}}
 #' @examples
 #' \dontrun{
@@ -79,15 +91,20 @@ get_branch <- function(repo, branch, ...) {
 #' auth_travis()
 #'
 #' # get settings
-#' get_repo_settings(repo = "cloudyr/travisci")
+#' g <- get_repo_settings(repo = "cloudyr/travisci")
 #'
-#' # specify new settings
+#' # pass modified settings list
+#' g[[1]] <- TRUE
+#' set_repo_settings(repo = "cloudyr/travisci", g)
+#'
+#' # specify new settings individually
 #' set_repo_settings(repo = "cloudyr/travisci", list("builds_only_with_travis_yml" = FALSE))
 #' }
 #' @seealso \code{\link{get_repo}}, \code{\link{get_env_vars}}
 #' @export
 get_repo_settings <- function(repo, ...) {
-    travisHTTP("GET", path = paste0("/repos/", repo, "/settings"), ...)
+    repo <- slug_to_id(repo)
+    travisHTTP("GET", path = paste0("/repos/", repo, "/settings"), ...)$settings
 }
 
 #' @rdname get_repo_settings
@@ -97,6 +114,12 @@ set_repo_settings <- function(repo, settings = list(), ...) {
     # build_pushes
     # build_pull_requests
     # maximum_number_of_builds
+    s <- c("builds_only_with_travis_yml",
+           "build_pushes",
+           "build_pull_requests",
+           "maximum_number_of_builds")
+    settings <- settings[names(settings) %in% s]
+    repo <- slug_to_id(repo)
     travisHTTP("PATCH", path = paste0("/repos/", repo, "/settings"), 
-               body = list(settings = settings), encode = "json", ...)
+               body = list(settings = settings), encode = "json", ...)$settings
 }
